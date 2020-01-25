38314b41b880d0e49b037926a19734bcdabfc6b0
cranpipe <cranpipe@users.noreply.github.com>
2020-01-25 20:06:30 -0300

    Versión #5.-

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+  $(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+
+  $(function() {
+    $('[data-toggle="popover"]').popover();
+  });
+
+
+  $(function() {
+    $("a").click(function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+
+        var gato = this.hash;
+        var diferencial = 0;
+
+        if (this.hash == "#aboutme") {
+          diferencial = 250;
+        }
+
+        if (this.hash == "#trabajos") {
+          diferencial = 70;
+        }
+
+        if (this.hash == "#contacto") {
+          diferencial = 13;
+        }
+        $("html, body").animate({
+          scrollTop: $(gato).offset().top - diferencial
+        }, 800);
+      }
+    });
+    $('[data-toggle="popover"]').popover();
+  });
+
+
+
+});
